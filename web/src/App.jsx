2934ed7640165416c7b48c31ff21ce869441e8d7
Damsiha Kumarasinghe <damsihak@gmail.com>
2025-10-11 22:56:18 +0530

    Add zustand state management for auth

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,24 +1,34 @@
-import Navbar from "./components/Navbar"
-import { Routes, Route } from "react-router-dom"
-import HomePage from "./pages/HomePage"
-import SignUpPage from "./pages/SignUpPage"
-import LogInPage from "./pages/LogInPage"
-import SettingsPage from "./pages/SettingsPage"
-import ProfilePage from "./pages/ProfilePage"
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LogInPage from "./pages/LogInPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
 
 const App = () => {
-  return (
-    <div>
-        <Navbar />
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route path="/login" element={<LogInPage/>} />
-            <Route path="/settings" element={<SettingsPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-        </Routes> 
-    </div>
-  )
+    const { authUser, checkAuth } = useAuthStore();
+
+    useEffect(() => {
+        checkAuth();
+    }, []);
+
+    console.log({ authUser });
+
+    return (
+        <div>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/signup" element={<SignUpPage />} />
+                <Route path="/login" element={<LogInPage/>} />
+                <Route path="/settings" element={<SettingsPage />} />
+                <Route path="/profile" element={<ProfilePage />} />
+            </Routes> 
+        </div>
+    )
 }
 
 export default App
