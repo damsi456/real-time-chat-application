f43dfbd685a9c414d2b0b9be90e7dcf66a330a7d
Damsiha Kumarasinghe <damsihak@gmail.com>
2025-10-26 16:19:08 +0530

    feat:Theme changing with UI preview in SettingsPage

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -9,10 +9,12 @@ import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
 
 import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
 import { useEffect } from "react";
 
 const App = () => {
     const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+    const { theme } = useThemeStore();
 
     useEffect(() => {
         checkAuth();
@@ -30,7 +32,7 @@ const App = () => {
     }
 
     return (
-        <div>
+        <div data-theme={theme}>
             <Navbar />
             <Routes>
                 <Route path="/" element={authUser ? <HomePage /> : <Navigate to="/login" />} />
---
+++ web/src/store/useThemeStore.js
@@ -0,0 +1,10 @@
+import { create } from "zustand";
+
+export const useThemeStore = create((set) => ({
+    theme: localStorage.getItem("chat-app-theme") || "light",
+    setTheme: (theme) => {
+        localStorage.setItem("chat-app-theme", theme)
+        set({ theme })
+        
+    }
+})) 
